3151c3a344ad7cdf61d392c23293d28cb299d075
Jon Clayden <code@clayden.org>
2022-12-22 14:54:03 +0000

    Removing inappropriate macro

--- R/array.R
+++ R/array.R
@@ -1,8 +1,8 @@
 #' Test the issue
 #' 
 #' This function just calls into C++ to convert its argument to a std::array,
-#' and then prints its elements. An error will be produced if \c vec doesn't
-#' have three elements or can't be converted to double.
+#' and then prints its elements. An error will be produced if vec doesn't have
+#' three elements or can't be converted to double.
 #' 
 #' @param vec Test vector
 #' @export
