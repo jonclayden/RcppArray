e3d7da6426ea8c018d17329ca40b17703d5affc5
Jon Clayden <code@clayden.org>
2022-12-22 15:46:27 +0000

    Back to the original approach

--- src/array.h
+++ src/array.h
@@ -7,41 +7,29 @@
 namespace Rcpp {
 namespace traits {
 
-// Forward declaration before including Rcpp.h
-template <typename ElementType, int Dimensionality>
-class Exporter<std::array<ElementType,Dimensionality>>;
-
-} }
-
-// Needed for Rcpp::Vector
-#include <Rcpp.h>
-
-namespace Rcpp {
-namespace traits {
-
 // Partial specialisation to allow as<array<T,D>>(...)
 template <typename ElementType, int Dimensionality>
 class Exporter<std::array<ElementType,Dimensionality>>
 {
 private:
-    const static int RTYPE = r_sexptype_traits<ElementType>::rtype;
-    Rcpp::Vector<RTYPE> vec;
-    
+    std::array<ElementType,Dimensionality> value;
+
 public:
-    Exporter (SEXP x) : vec(x) {}
-    
-    std::array<ElementType,Dimensionality> get ()
+    Exporter (SEXP x)
     {
+        std::vector<ElementType> vec = as<std::vector<ElementType>>(x);
         if (vec.size() != Dimensionality)
             throw Rcpp::exception("Array does not have the expected number of elements");
-        std::array<ElementType,Dimensionality> result;
         for (int i=0; i<Dimensionality; i++)
-            result[i] = vec[i];
-        return result;
+            value[i] = vec[i];
     }
+    
+    std::array<ElementType,Dimensionality> get () { return value; }
 };
 
 } // traits namespace
 } // Rcpp namespace
+
+#include <Rcpp.h>
 
 #endif
