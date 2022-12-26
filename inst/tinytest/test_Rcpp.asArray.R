abd5359f09a7fe34faec5f2fa0c2328d888d4a22
Jon Clayden <code@clayden.org>
2022-12-26 18:35:27 +0100

    Adding tests reflecting the current code

--- inst/tinytest/test_Rcpp.asArray.R
+++ inst/tinytest/test_Rcpp.asArray.R
@@ -1,4 +1,8 @@
+expect_stdout(Rcpp.asArray:::foo(), "1 2 3")
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+expect_stdout(Rcpp.asArray:::intArray(1:3), "1 2 3")
+expect_error(Rcpp.asArray:::intArray(1:4), "expected number of elements")
 
+expect_stdout(Rcpp.asArray:::doubleArray(runif(3)), "0.")
+
+expect_error(Rcpp.asArray:::int64Array(1:3), "Wrong R type")
