e6cdb9c4e3556f15468ff4cd9cb16b3177e09ea0
Jon Clayden <code@clayden.org>
2023-01-25 12:40:33 +0000

    Adding initial code for std::span

--- inst/include/RcppArray.h
+++ inst/include/RcppArray.h
@@ -4,6 +4,11 @@
 #include <RcppCommon.h>
 #include <array>
 
+#if __cplusplus >= 202002L && defined(__cpp_lib_span)
+#define HAVE_SPAN
+#include <span>
+#endif
+
 namespace Rcpp {
   namespace traits {
 
@@ -12,6 +17,14 @@ namespace Rcpp {
 
     template <typename T, std::size_t D> SEXP wrap(const std::array<T,D>&);
 
+#ifdef HAVE_SPAN
+    
+    // Ditto for std::span, which is from C++20
+    template <typename T, std::size_t D> class Exporter<std::span<T,D>>;
+    
+    template <typename T, std::size_t D> SEXP wrap(const std::span<T,D>&);
+    
+#endif
   }
 }
 
@@ -42,6 +55,33 @@ namespace Rcpp {
       const int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype;
       return Vector<RTYPE>(object.begin(), object.end());
     }
+    
+#ifdef HAVE_SPAN
+    
+    template <typename T, std::size_t D> class Exporter<std::span<T,D>> {
+      typedef typename std::span<T,D> STD;
+    
+      const static int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype;
+      Rcpp::Vector<RTYPE> vec;
+    
+    public:
+      // std::span is a view, so does type conversion make sense semantically?
+      // Does as<span<T>>(...) even make sense?
+      Exporter(SEXP x): vec(x) {
+        if (D != std::dynamic_extent && vec.size() != D) Rcpp::stop("Span does not have the expected number of elements");
+      }
+      STD get() {
+        // Initialisation of a sized span (D neither 0 nor dynamic) isn't allowed without argument
+        return STD(vec.begin(), vec.end());
+      }
+    };
+    
+    template <typename T, std::size_t D> SEXP wrap(const std::span<T,D>& object) {
+      const int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype;
+      return Vector<RTYPE>(object.begin(), object.end());
+    }
+    
+#endif
   }
 }
 
