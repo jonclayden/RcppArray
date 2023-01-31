7dfae48177d8287de89d08a0aa0af352fb6a1ae5
Jon Clayden <code@clayden.org>
2023-01-31 16:57:00 +0000

    Adding basic package self-documentation page

--- R/array.R
+++ R/array.R
@@ -1,3 +1,14 @@
 #' @importFrom Rcpp evalCpp
 #' @useDynLib RcppArray
 NULL
+
+#' The 'RcppArray' package
+#' 
+#' This package presents no user-facing R code, but instead provides linked
+#' packages containing C++ code with the ability to convert easily between R
+#' vector types and the \code{std::array} and \code{std::span} classes. See
+#' the package README and/or included header \code{RcppArray.h} for further
+#' details.
+#' 
+#' @keywords internal
+"_PACKAGE"
