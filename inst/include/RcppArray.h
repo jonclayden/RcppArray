8e22b6de0ccae5c20903291616a1f99dad758512
Jon Clayden <code@clayden.org>
2023-11-03 21:44:40 +0000

    Adding support for std::tuple (cf. https://github.com/RcppCore/Rcpp/issues/1278) without post-C++11 tricks

--- inst/include/RcppArray.h
+++ inst/include/RcppArray.h
@@ -3,6 +3,7 @@
 
 #include <RcppCommon.h>
 #include <array>
+#include <tuple>
 
 #if !defined(HAVE_SPAN) && __cplusplus >= 202002L && defined(__cpp_lib_span)
 #define HAVE_SPAN
@@ -20,6 +21,11 @@ namespace Rcpp {
 
     template <typename T, std::size_t D> SEXP wrap(const std::array<T,D>&);
 
+    // Ditto for std::tuple
+    template <typename... T> class Exporter<std::tuple<T...>>;
+    
+    template <typename... T> SEXP wrap(const std::tuple<T...>&);
+
 #ifdef HAVE_SPAN
     
     // Ditto for std::span, which is from C++20
@@ -33,13 +39,52 @@ namespace Rcpp {
 
 #include <Rcpp.h>
 
+namespace RcppArray {
+  namespace internal {
+    // Helper class to copy between a std::tuple and an R list. Each element
+    // has its own specialisation, which contains a child element that handles
+    // the previous element, down to element zero which terminates the chain
+    template <std::size_t N, typename... T> struct Copier {
+      typedef typename std::tuple<T...> TT;
+      
+    private:
+      TT& tuple;
+      Rcpp::List& list;
+      Copier<N-1, T...> child;
+      
+    public:
+      Copier(TT& t, Rcpp::List& l): tuple(t), list(l), child(t,l) {}
+      
+      void list2tuple() const {
+        std::get<N-1>(tuple) = Rcpp::as<typename std::tuple_element<N-1,TT>::type>(list[N-1]);
+        child.list2tuple();
+      }
+      
+      void tuple2list() const {
+        list[N-1] = std::get<N-1>(tuple);
+        child.tuple2list();
+      }
+    };
+    
+    // Zeroth-level copier does nothing
+    template <typename... T> struct Copier<0, T...> {
+      typedef typename std::tuple<T...> TT;
+      
+      Copier(TT& t, Rcpp::List& l) {}
+      
+      void list2tuple() const {}
+      void tuple2list() const {}
+    };
+  }
+}
+
 namespace Rcpp {
   namespace traits {
 
     template <typename T, std::size_t D> class Exporter<std::array<T,D>> {
       typedef typename std::array<T,D> ATD;
 
-      // Convert the type to a valid rtype.
+      // Convert the type to a valid R type
       const static int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype;
       Rcpp::Vector<RTYPE> vec;
 
@@ -57,6 +102,31 @@ namespace Rcpp {
     template <typename T, std::size_t D> SEXP wrap(const std::array<T,D>& object) {
       const int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype;
       return Vector<RTYPE>(object.begin(), object.end());
+    }
+    
+    template <typename... T> class Exporter<std::tuple<T...>> {
+      typedef typename std::tuple<T...> TT;
+    
+      // Tuples are (in general) a mixed type, analogous to an R list
+      Rcpp::List list;
+    
+    public:
+      Exporter(SEXP x): list(x) {
+        if (list.size() != sizeof...(T)) Rcpp::stop("Tuple does not have the expected number of elements");
+      }
+      TT get() {
+        TT x;
+        RcppArray::internal::Copier<sizeof...(T), T...> copier(x, list);
+        copier.list2tuple();
+        return x;
+      }
+    };
+    
+    template <typename... T> SEXP wrap(const std::tuple<T...>& object) {
+      Rcpp::List list(sizeof...(T));
+      RcppArray::internal::Copier<sizeof...(T), T...> copier(object, list);
+      copier.tuple2list();
+      return list;
     }
     
 #ifdef HAVE_SPAN
